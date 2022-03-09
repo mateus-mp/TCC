e25216c119ab5014685571edf66258eb438e7e2a
Mateus <mateusmanoel2009@hotmail.com>
2022-03-09 19:10:25 -0300

    Update test.c

--- test.c
+++ test.c
@@ -2,8 +2,9 @@
 #include <unistd.h>
 
 int main(int argc, char *argv[]) {
-    FILE *file = fopen("/home/beringela/teste/teste.txt", "w");
+    FILE *file = fopen("/home/beringela/teste/teste.txt", "a");
     sleep(5);
+    //fprintf(file, "append");
     fclose(file);
     return 0;
 }
